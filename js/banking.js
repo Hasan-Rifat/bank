9034b8840079d92ec33b7c123da2a7ee870b6e7d
developerhr000 <80412774+developerhr000@users.noreply.github.com>
2022-02-17 02:30:09 +0600

    done

--- js/banking.js
+++ js/banking.js
@@ -4,59 +4,72 @@
 }
  */
 
-function getInputValue() {
-    const depositInput = document.getElementById('deposit-input');
-    const depositAmount = parseFloat(depositInput.value);
-    depositInput.value = "";
-    return depositAmount;
-    
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const amountValue = parseFloat(inputField.value);
+    inputField.value = "";
+    return amountValue;
 }
 
+function getAmountValue(amount, depositAmount) {
+    const money = document.getElementById(amount);
+    const currentAmount = parseFloat(money.innerText);
+    money.innerText = currentAmount + depositAmount;
+}
 
-document.getElementById('deposit-button').addEventListener('click', function () {
-    /*    const depositInput = document.getElementById('deposit-input');
-       const depositAmount = parseFloat(depositInput.value); */
-    // depositAmount total
-    const depositAmount = getInputValue();
-
-    const depositTotal = document.getElementById('deposit-total');
-    const currentAmount = parseFloat(depositTotal.innerText);
-
-    const depositMainAmount = currentAmount + depositAmount;
-    depositTotal.innerText = depositMainAmount;
-
-
-
-    // step 2 end 
-    // update blance 
+function updateBalance(depositAmount) {
     const blanca = document.getElementById('balance-total');
     const blancaTotal = parseFloat(blanca.innerText);
-    // blanca.innerText = blancaTotal;
-    const updateBlance = blancaTotal + depositAmount;
-    blanca.innerText = updateBlance;
-    
+    if (isAdd == true) {
+        blanca.innerText = blancaTotal + depositAmount;
+    } else {
+        blanca.innerText = blancaTotal - depositAmount;
+    }
+}
+
+function updateBalance(depositAmount) {
+    const withdrawNow = document.getElementById('balance-total');
+    const withdrawNowAmount = parseFloat(withdrawNow.innerText);
+    const withdrawNowMainAmount = withdrawNowAmount - withdrawAmount;
+    withdrawNow.innerText = withdrawNowMainAmount;
+}
+
+document.getElementById('deposit-button').addEventListener('click', function () {
+
+    const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0) {
+        totalMoney = getAmountValue('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
+
+
 });
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('withdraw-input');
-    const withdrawAmount = parseFloat(withdrawInput.value);
-    // depositAmount total
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const currentWithdrawAmount = parseFloat(withdrawTotal.innerText);
 
-    const withdrawMainAmount = currentWithdrawAmount + withdrawAmount;
-    withdrawTotal.innerText = withdrawMainAmount;
+    // updateBalance(depositAmount, false);
 
-    const withdrawNow = document.getElementById('balance-total');
-    const withdrawNowAmount = parseFloat(withdrawNow.innerText);
+    /* const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmount = parseFloat(withdrawInput.value) */
+    ;
+    const withdrawAmount = getInputValue('withdraw-input');
+    if (withdrawAmount > 0) {
+        // depositAmount total
+        const withdrawTotal = document.getElementById('withdraw-total');
+        const currentWithdrawAmount = parseFloat(withdrawTotal.innerText);
 
-    const withdrawNowMainAmount = withdrawNowAmount - withdrawAmount;
-    withdrawNow.innerText = withdrawNowMainAmount;
+        const withdrawMainAmount = currentWithdrawAmount + withdrawAmount;
+        withdrawTotal.innerText = withdrawMainAmount;
 
-    if (withdrawNow.innerText == -1) {
-        withdrawNow.innerText = 0;
+        const withdrawNow = document.getElementById('balance-total');
+        const withdrawNowAmount = parseFloat(withdrawNow.innerText);
+
+        const withdrawNowMainAmount = withdrawNowAmount - withdrawAmount;
+        withdrawNow.innerText = withdrawNowMainAmount;
     }
-    withdrawInput.value = "";
+
+
+
 });
 
 // blanca filed 
